df4d0cdafe5eb2a4fb8d207e997c863a340e39da
ismaelsadeghicode <ismaelsadeghicode@gmail.com>
2020-08-29 16:02:47 +0430

    add UploadDownloadingApplication & UploadDownloadingApplicationTests

---
+++ src/main/java/com/javatar/uploaddownloading/UploadDownloadingApplication.java
@@ -0,0 +1,13 @@
+package com.javatar.uploaddownloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadDownloadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatar/uploaddownloading/UploadDownloadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatar.uploaddownloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDownloadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
